7e8555ad66ef0c07243fa92b4e78b4e26f436dba
Prabhat Kumar <prabhatkr0099@gmail.com>
2021-12-21 18:57:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import User from './User';
+import './style.css';
+import React from "react";
+function App(){
+    return(
+        <div>
+            <User />
+        </div>
+    )
+}
+export default App;
